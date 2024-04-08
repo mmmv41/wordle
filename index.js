045de48fd31ffd89179e35021db2beaa28027b54
mmmv41 <tlsalsrud5068@daum.net>
2024-04-08 14:12:44 +0900

    [워들] index.js 수정
    주석 처리 나중에 하기

--- index.js
+++ index.js
@@ -1,38 +1,118 @@
-let index = 0; // let 으로 지정 -> 수정 가능하다
-let attempts = 0; //몇번 시도했는가
+const 정답 = "APPLE";
+let attempts = 0;
+let index = 0;
+let timer;
 
 function appStart() {
-  const handleEnterKey = () => {
-    //정답확인
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임이 종료됐습니다.";
+    div.style =
+      "display:flex; justify-content:center; align-items:center; position:absolute; top:15vh; left:35vw; background-color:white; width:200px; height:100px;";
+    div.classList.add("fade-in");
+    document.body.appendChild(div);
   };
-}
+  const gameover = () => {
+    window.removeEventListener("keydown", handleKeydown);
+    displayGameover();
+    clearInterval(timer);
+  };
 
+  const nextLine = () => {
+    if (attempts >= 5) {
+      gameover();
+      return;
+    } else {
+      attempts += 1;
+      index = 0;
+    }
+  };
+
+  const handleEnterKey = () => {
+    let 맞은_갯수 = 0;
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-block[data-index='${attempts}${i}']`
+      );
+      const 입력_글자 = block.innerText;
+      const 정답_글자 = 정답[i];
+      if (입력_글자 === 정답_글자) {
+        맞은_갯수 += 1;
+        block.style.background = "#6AAA64";
+      } else if (정답.includes(입력_글자)) {
+        block.style.background = "#C9B458";
+      } else block.style.background = "#787C7E";
+      block.style.color = "white";
+    }
+    if (맞은_갯수 === 5) gameover();
+    else nextLine();
+  };
+
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-block[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index != 0) index -= 1;
+  };
   const handleKeydown = (event) => {
+    if (attempts >= 6) return;
     const key = event.key.toUpperCase();
-    //key에 event.key 값 지정
-    //toUpperCase() : 대문자로 변환
-    const keyCode = event.keyCode; //keyCode에 event.keyCode 값 지정
+    const keyCode = event.keyCode || key.charCodeAt(0);
     const thisBlock = document.querySelector(
       `.board-block[data-index='${attempts}${index}']`
     );
-    if (event.key === "Enter") {
-      handleEnterKey();
-    } else if (index === 5) return;
-    else if (keyCode >= 65 && keyCode <= 90) {
+
+    if (event.key === "Backspace") handleBackspace(thisBlock);
+    else if (index === 5) {
+      if (event.key === "Enter") {
+        handleEnterKey();
+      } else return;
+    } else if (65 <= keyCode && keyCode <= 90) {
       thisBlock.innerText = key;
       index += 1;
     }
+  };
 
+  const startTimer = () => {
+    const 시작_시간 = new Date();
 
-    /*if (keyCode >= 65 && keyCode <= 90) {
-      //keyCode가 65~90사이인 값만 들어가게 (a~z 알파벳만 들어가게)
-      thisBlock.innerText = key;
-      //입력하는 key값을 첫번째 블럭에 넣는다 (innerText)
-      index += 1; // index = index + 1, index++, index += 1다 같은 표현
+    function setTime() {
+      const 현재_시간 = new Date();
+      const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+      const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+      const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${분}:${초}`;
     }
+    timer = setInterval(setTime, 1000);
+  };
 
-    //console.log(event.key, event.keyCode); //event 무슨 키를 눌렀는지 , 몇번째 키코드인지
-  };*/
+  function setClickEvents() {
+    const keys = document.querySelectorAll(
+      ".keyboard-alphabet, .keyboard-alphabet-bigsize div"
+    );
+    keys.forEach((key) => {
+      key.addEventListener("click", () => {
+        const keyValue = key.getAttribute("data-key");
+
+        if (keyValue === "ENTER") {
+          handleEnterKey();
+        } else if (keyValue === "CANCEL") {
+          handleBackspace();
+        } else {
+          const eventMock = { key: keyValue, keyCode: keyValue.charCodeAt(0) };
+          handleKeydown(eventMock);
+        }
+      });
+    });
+  }
+
+  startTimer();
+  setClickEvents();
+  window.addEventListener("keydown", handleKeydown);
   window.addEventListener("keydown", handleKeydown);
 }
 
